d628f44532f7d152b496cfd9427bb4ad499b308d
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2023-05-28 23:10:02 +0530

    Initial commit

---
+++ src/atm/Atm.java
@@ -0,0 +1,19 @@
+package atm;
+
+public abstract class Atm {
+
+    private static double atmBalance = 100000;
+
+    public abstract void cashWithdrawal(String accountNo);
+    public abstract void fundTransfer(String accountNo);
+    public abstract void checkBalance(String accountNo);
+
+    public static double getAtmBalance() {
+        return atmBalance;
+    }
+
+    public void setAtmBalance(double atmBalance) {
+        this.atmBalance = atmBalance;
+    }
+
+}
